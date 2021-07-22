a7b1b30a6e16495c14fd08d46dc0952693827306
nmikle <nekrasovmikle@gmail.com>
2021-07-22 20:37:14 +0300

    object equals/hashcode

--- src/main/java/com/epam/jwd/app/Application.java
+++ src/main/java/com/epam/jwd/app/Application.java
@@ -1,23 +1,30 @@
 package com.epam.jwd.app;
 
-import com.epam.jwd.math.CustomMath;
+import com.epam.jwd.model.User;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Application {
 
-	private static final Logger LOG = LogManager.getLogger(Application.class);
+    private static final Logger LOG = LogManager.getLogger(Application.class);
 
-	public static void main(String[] args) {
-		LOG.trace("Program start");
-		if (args.length < 2) {
-			LOG.error("Not enough arguments");
-			System.exit(-1);
-		}
-		int a = Integer.valueOf(args[0]);
-		int b = Integer.valueOf(args[1]);
-		int result = CustomMath.crazySum(a, b);
-		LOG.info("Result is {}", result);
-		LOG.trace("Program end");
-	}
+    public static void main(String[] args) {
+        Object human = new User("Paul", 12);
+        Object anotherHuman = new User("Paul", 12);
+        Object thirdHuman = new User("Paul", 12);
+
+        System.out.println(human.equals(human)); //ref
+
+        System.out.println(anotherHuman.equals(human));//sym
+        System.out.println(human.equals(anotherHuman));//sym
+
+        System.out.println(human.equals(anotherHuman));
+        System.out.println(anotherHuman.equals(thirdHuman));
+        System.out.println(human.equals(thirdHuman)); //trans
+
+        System.out.println(human.equals(null)); //no NPE
+
+        System.out.println(human.hashCode());
+        System.out.println(human);
+    }
 }
