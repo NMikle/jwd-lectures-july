8be01e1e58c8814796afd6e9736752c4ae8373ec
nmikle <nekrasovmikle@gmail.com>
2021-07-29 20:49:13 +0300

    enums capabilities

--- src/main/java/com/epam/jwd/app/Application.java
+++ src/main/java/com/epam/jwd/app/Application.java
@@ -1,38 +1,31 @@
 package com.epam.jwd.app;
 
-import com.epam.jwd.model.Animal;
-import com.epam.jwd.model.Cat;
-import com.epam.jwd.model.Dog;
-import com.epam.jwd.model.Mammal;
-import com.epam.jwd.model.Tail;
-import com.epam.jwd.service.AnimalService;
+import com.epam.jwd.model.Color;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Application {
 
     private static final Logger LOG = LogManager.getLogger(Application.class);
-    private static final int DEFAULT_WOOL_LENGTH = 1;
-    private static final String TEST_DOG_NAME = "Bobik";
-    private static final int TEST_DOG_AGE = 2;
-    private static final int TEST_DOG_TAIL_LENGTH = 3;
-    private static final int CUSTOM_TAIL_WOOL_LENGTH_FOR_TEST = 2;
 
     public static void main(String[] args) {
+        System.out.println("Program Start");
+        final Color color = Color.of("fF0000");
+        System.out.println(color);
+    }
 
-        Animal bobik = new Dog(TEST_DOG_NAME, TEST_DOG_AGE,
-                TEST_DOG_TAIL_LENGTH, new Tail(CUSTOM_TAIL_WOOL_LENGTH_FOR_TEST));
-        final Cat barsik = new Cat("Barsik", 8);
-//        Animal cat = new Animal("Barsik", 4); // Does not work if Animal is abstract!
-
-        final AnimalService animalService = new AnimalService();
-
-        animalService.makeAnimalDoNoises(bobik);
-        animalService.makeAnimalDoNoises(barsik);
-
-        animalService.makeMammalMove(bobik);
-        animalService.makeMammalMove(barsik);
-
+    private static void method(Color color) {
+        switch (color) {
+            case GREEN:
+            case RED:
+                System.out.println("Not interesting");
+                break;
+            case BLUE:
+                System.out.println("blue");
+                break;
+            default:
+                throw new RuntimeException("not implemented");
+        }
     }
 
 }
---
+++ src/main/java/com/epam/jwd/model/Color.java
@@ -0,0 +1,44 @@
+package com.epam.jwd.model;
+
+public enum Color {
+    RED("FF0000"),
+    GREEN("00FF00"),
+    BLUE("0000FF");
+
+    private static final String COLOR_NOT_FOUND_MSG = "%s does not have representation in %s";
+
+//    private static final Map<String, Color> cache = new HashMap<>();
+//
+//    static {
+//        System.out.println("static start");
+//        for (Color color : values()) {
+//            cache.put(color.getRgbValue(), color);
+//        }
+//        System.out.println("static end");
+//    }
+
+    private final String rgbValue;
+
+    Color(String rgbValue) {
+        System.out.printf("Initializing %s Color constant with ordinal %s\n", name(), ordinal());
+        this.rgbValue = rgbValue;
+    }
+
+    public String getRgbValue() {
+        return rgbValue;
+    }
+
+    public static Color of(String rgbValue) {
+//        final Color result = cache.get(rgbValue.toUpperCase(Locale.ROOT));
+//        if (result == null) {
+//            throw new IllegalArgumentException(String.format(COLOR_NOT_FOUND_MSG, rgbValue, Color.class.getSimpleName()));
+//        }
+//        return result;
+        for (Color color : Color.values()) {
+            if (color.rgbValue.equalsIgnoreCase(rgbValue)) {
+                return color;
+            }
+        }
+        throw new IllegalArgumentException(String.format(COLOR_NOT_FOUND_MSG, rgbValue, Color.class.getSimpleName()));
+    }
+}
