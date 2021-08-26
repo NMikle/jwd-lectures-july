5f206c52e3120cb8f0396e03e9615d3619edd677
nmikle <nekrasovmikle@gmail.com>
2021-08-26 19:26:19 +0300

    collections; enumset

--- src/main/java/com/epam/jwd/app/Application.java
+++ src/main/java/com/epam/jwd/app/Application.java
@@ -1,16 +1,17 @@
 package com.epam.jwd.app;
 
+import com.epam.jwd.model.Color;
 import com.epam.jwd.model.Department;
 import com.epam.jwd.model.Employee;
-import com.epam.jwd.model.User;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
+import java.util.EnumSet;
+import java.util.HashSet;
 import java.util.List;
-import java.util.function.Function;
+import java.util.Set;
 
 public class Application {
 
@@ -19,7 +20,7 @@ public class Application {
     public static void main(String[] args) {
         final Department market = new Department("Marketing");
         final Department sales = new Department("Sales");
-        final List<Employee> marketingDept = new ArrayList<>();
+        List<Employee> marketingDept = new ArrayList<>();
 
         marketingDept.add(new Employee(1, "Bob", 23, market));
         marketingDept.add(new Employee(2, "Alice", 25, market));
@@ -38,12 +39,20 @@ public class Application {
 //            }
 //        })); //comparator with anonymous class (for functional interface)
 //        marketingDept.sort(Comparator.comparing(emp -> emp.getName()));// same as previous
+//        marketingDept = Collections.unmodifiableList(marketingDept); //will make marketingDept unmodifiable
         marketingDept.sort(Comparator.comparing(Employee::getDepartment));// same as previous
 
 
         for (Employee employee : marketingDept) {
             System.out.println(employee);
         }
+
+//        Set<Color> colors = EnumSet.allOf(Color.class);
+//        for (Color color : colors) {
+//            System.out.println(color);
+//        }
+
+//        final List<Integer> ints = Arrays.asList(3, 4, 5);//Immutable collection
     }
 
     private static class Test {
