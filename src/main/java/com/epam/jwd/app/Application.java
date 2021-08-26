2184b4f6c597715c783891c0f8ad42e3f1e7430a
nmikle <nekrasovmikle@gmail.com>
2021-08-26 21:21:39 +0300

    stream api

--- src/main/java/com/epam/jwd/app/Application.java
+++ src/main/java/com/epam/jwd/app/Application.java
@@ -2,12 +2,19 @@ package com.epam.jwd.app;
 
 import com.epam.jwd.model.Department;
 import com.epam.jwd.model.Employee;
+import com.epam.jwd.model.User;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toSet;
 
 public class Application {
 
@@ -16,62 +23,44 @@ public class Application {
     public static void main(String[] args) {
         final Department market = new Department("Marketing");
         final Department sales = new Department("Sales");
-        List<Employee> marketingDept = new ArrayList<>();
+        final Department it = new Department("IT");
+        List<Employee> employees = new ArrayList<>();
 
-        marketingDept.add(new Employee(1, "Bob", 23, market));
-        marketingDept.add(new Employee(2, "Alice", 25, sales));
-        marketingDept.add(new Employee(1, "Tom", 22, market));
+        employees.add(new Employee(1, "Bob", 23, market));
+        employees.add(new Employee(2, "Bob", 27, market));
+        employees.add(new Employee(3, "Alice", 25, sales));
+        employees.add(new Employee(4, "Tom", 22, market));
+        employees.add(new Employee(5, "Rob", 21, sales));
+        employees.add(new Employee(6, "Mark", 11, sales));
+        employees.add(new Employee(7, "Mary", 65, it));
+        employees.add(new Employee(8, "Alex", 81, it));
 
-        int count = 0;
-//        count++; //breaks effectively final
-        marketingDept.forEach(emp -> {
-            System.out.println(emp);
-            System.out.println(count);
+        final Map<Department, Set<Employee>> employeesByDepartments = employees.stream()
+                .collect(groupingBy(Employee::getDepartment, toSet()));
+
+        employeesByDepartments.keySet().forEach(key -> {
+            employeesByDepartments.get(key).forEach(System.out::println);
+            System.out.println();
         });
 
-        System.out.println();
+//        marketingDept.forEach(System.out::println);
 
-//        marketingDept.sort(Comparator.comparing(new Function<Employee, String>() {
-//            @Override
-//            public String apply(Employee emp) {
-//                return employee.getName();
+        final Date start = new Date();
+        final Set<String> names = employees.stream()
+                .filter(emp -> emp.getAge() > 22)
+                .map(User::getName)
+                .collect(toSet());
+
+//        final Set<String> names = new HashSet<>();
+//        for (Employee emp : employees) {
+//            if (emp.getAge() > 22) {
+//                String name = emp.getName();
+//                names.add(name);
 //            }
-//        })); //comparator with anonymous class (for functional interface)
-//        marketingDept.sort(Comparator.comparing(emp -> emp.getName()));// same as previous
-        marketingDept.sort(Comparator.comparing(Employee::getDepartment));// same as previous
-
-//        marketingDept.forEach(emp -> System.out.println(emp));
-        marketingDept.forEach(System.out::println);
-
-
-//        for (Employee employee : marketingDept) {
-//            System.out.println(employee);
 //        }
+        System.out.printf("time passed: %s\n", new Date().getTime() - start.getTime());
 
-//        Set<Color> colors = EnumSet.allOf(Color.class);
-//        for (Color color : colors) {
-//            System.out.println(color);
-//        }
-
-//        final List<Integer> ints = Arrays.asList(3, 4, 5);//Immutable collection
-
-//        doSomething(i -> String.valueOf(i));
-//        doSomething(String::valueOf);
-//        doSomething(Application::convertAndAddSymbols);
+        names.forEach(System.out::println);
     }
 
-    private static void doSomething(CustomFunctionalInterface functionalInterface) {
-        final String result = functionalInterface.theOnlyAbstractMethod(4);
-        System.out.printf("This was converted to string: %s.\n", result);
-    }
-
-    private static String convertAndAddSymbols(int value) {
-        return "\"" + value + "\"";
-    }
-
-    private interface CustomFunctionalInterface {
-
-        String theOnlyAbstractMethod(int i);
-
-    }
 }
