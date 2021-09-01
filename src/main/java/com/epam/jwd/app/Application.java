20b776e361bf9a218f5f3e85adf88bff3a57c3b1
nmikle <nekrasovmikle@gmail.com>
2021-09-01 21:13:15 +0300

    references

--- src/main/java/com/epam/jwd/app/Application.java
+++ src/main/java/com/epam/jwd/app/Application.java
@@ -1,33 +1,23 @@
 package com.epam.jwd.app;
 
+import com.epam.jwd.model.Employee;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.lang.ref.SoftReference;
+import java.lang.ref.WeakReference;
 
 public class Application {
 
     private static final Logger LOG = LogManager.getLogger(Application.class);
 
     public static void main(String[] args) {
-//        final ExceptionFromStaticField instance = ExceptionFromStaticField.instance;
-        final String input = "Hello, my phone number is: +375(33)355-27-33. Please call me in case of any questions.";
-        final String anotherInput = "Phone number is: +3752955428157.";
-        final Pattern pattern = Pattern.compile("\\+375\\s?((\\(25\\)|(25))|(\\(29\\)|(29))|(\\(33\\)|(33))|(\\(44\\)|(44)))\\s?\\d{3}-?\\d{2}-?\\d{2}(?=\\D)");
-        final Matcher matcher = pattern.matcher(input);
-        System.out.println(matcher.groupCount());
-        System.out.println(matcher.find());
-        System.out.println(matcher.start());
-        System.out.println(matcher.end());
-        System.out.println(matcher.group());
-        System.out.println(matcher.group(1));
-
-        matcher.reset(anotherInput);
-        if (matcher.find()) {
-            System.out.println(matcher.group());
-            System.out.println(matcher.group(1));
-        }
+        SoftReference<Employee> softlyReachableBob = new SoftReference<>(
+                new Employee(1, "Bob", 42, null)
+        );
+        System.out.println(softlyReachableBob.get());
+        final WeakReference<String> weaklyReachableStr = new WeakReference<>("Hello World");
+        System.out.println(weaklyReachableStr.get());
     }
 
 }
