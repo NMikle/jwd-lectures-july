a46e6a1b79278e4bbcba64bc97dee6313ce98359
nmikle <nekrasovmikle@gmail.com>
2021-09-01 19:48:03 +0300

    regex deeper

--- src/main/java/com/epam/jwd/app/Application.java
+++ src/main/java/com/epam/jwd/app/Application.java
@@ -1,6 +1,5 @@
 package com.epam.jwd.app;
 
-import com.epam.jwd.model.ExceptionFromStaticField;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -13,11 +12,22 @@ public class Application {
 
     public static void main(String[] args) {
 //        final ExceptionFromStaticField instance = ExceptionFromStaticField.instance;
-        final Pattern pattern = Pattern.compile("[a-z]+([1-9])");
-        final Matcher matcher = pattern.matcher("input34");
+        final String input = "Hello, my phone number is: +375(33)355-27-33. Please call me in case of any questions.";
+        final String anotherInput = "Phone number is: +3752955428157.";
+        final Pattern pattern = Pattern.compile("\\+375\\s?((\\(25\\)|(25))|(\\(29\\)|(29))|(\\(33\\)|(33))|(\\(44\\)|(44)))\\s?\\d{3}-?\\d{2}-?\\d{2}(?=\\D)");
+        final Matcher matcher = pattern.matcher(input);
         System.out.println(matcher.groupCount());
-        System.out.println(matcher.lookingAt());
-//        System.out.println(Pattern.matches("[a-z]+", "input34"));
+        System.out.println(matcher.find());
+        System.out.println(matcher.start());
+        System.out.println(matcher.end());
+        System.out.println(matcher.group());
+        System.out.println(matcher.group(1));
+
+        matcher.reset(anotherInput);
+        if (matcher.find()) {
+            System.out.println(matcher.group());
+            System.out.println(matcher.group(1));
+        }
     }
 
 }
