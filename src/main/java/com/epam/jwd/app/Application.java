1063457a21e32a736803f96b717a9ad37d896b25
nmikle <nekrasovmikle@gmail.com>
2021-09-02 19:34:04 +0300

    java.util.function package

--- src/main/java/com/epam/jwd/app/Application.java
+++ src/main/java/com/epam/jwd/app/Application.java
@@ -1,23 +1,27 @@
 package com.epam.jwd.app;
 
-import com.epam.jwd.model.Employee;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.lang.ref.SoftReference;
-import java.lang.ref.WeakReference;
+import java.util.function.Consumer;
 
 public class Application {
 
     private static final Logger LOG = LogManager.getLogger(Application.class);
 
     public static void main(String[] args) {
-        SoftReference<Employee> softlyReachableBob = new SoftReference<>(
-                new Employee(1, "Bob", 42, null)
-        );
-        System.out.println(softlyReachableBob.get());
-        final WeakReference<String> weaklyReachableStr = new WeakReference<>("Hello World");
-        System.out.println(weaklyReachableStr.get());
+        final Consumer<String> composedConsumer = test().andThen(x -> {
+            System.out.println("second consumer");
+            System.out.println(x);
+        });
+        composedConsumer.accept("Hello");
+    }
+
+    private static Consumer<String> test() {
+        return x -> {
+            System.out.println("first consumer");
+            System.out.println(x);
+        };
     }
 
 }
